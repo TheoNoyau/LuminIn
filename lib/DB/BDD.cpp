6440f634fa7e7bf7bb46cc18d68ae5c123d0da16
“Aymeric <aymeric.lavedrine@etu.univ-amu.fr>
2020-04-08 16:45:16 +0200

    Added function deleteEntry for Company and JobSeeker

--- lib/DB/BDD.cpp
+++ lib/DB/BDD.cpp
@@ -541,10 +541,76 @@ void updateEntry(Job &j)
 
 void deleteEntry(Company &c)
 {
+    string filenameIn = dbPath + "/companies.csv";
+    string filenameOut = dbPath + "/tmp.csv";
+    const char * cIn = filenameIn.c_str();
+    const char * cOut = filenameOut.c_str();
+    ifstream db(cIn);
+    ofstream dbNew;
+    dbNew.open(cOut, ios::out);
+
+    vector<string> dataLine;
+    string row, data, temp;
+    int companyId = c.getId(), id;
+
+    getline(db, temp) ;
+    dbNew << temp << "\n";   
+    while (getline(db, row))
+    {
+        dataLine.clear();
+        stringstream s(row) ;
+
+        while (getline(s, data, ',')) {
+            dataLine.push_back(data) ;
+        }
+
+        id = stoi(dataLine[0]) ;
+        if (companyId != id) {
+            dbNew << row << "\n";
+        }
+    }
+
+    db.close();
+    dbNew.close();
+    remove(cIn);
+    rename(cOut, cIn); 
 }
 
 void deleteEntry(JobSeeker &js)
 {
+    string filenameIn = dbPath + "/jobseekers.csv";
+    string filenameOut = dbPath + "/tmp.csv";
+    const char * cIn = filenameIn.c_str();
+    const char * cOut = filenameOut.c_str();
+    ifstream db(cIn);
+    ofstream dbNew;
+    dbNew.open(cOut, ios::out);
+
+    vector<string> dataLine;
+    string row, data, temp;
+    int companyId = c.getId(), id;
+
+    getline(db, temp) ;
+    dbNew << temp << "\n";   
+    while (getline(db, row))
+    {
+        dataLine.clear();
+        stringstream s(row) ;
+
+        while (getline(s, data, ',')) {
+            dataLine.push_back(data) ;
+        }
+
+        id = stoi(dataLine[0]) ;
+        if (companyId != id) {
+            dbNew << row << "\n";
+        }
+    }
+
+    db.close();
+    dbNew.close();
+    remove(cIn);
+    rename(cOut, cIn);
 }
 
 void deleteEntry(Employee &e)
