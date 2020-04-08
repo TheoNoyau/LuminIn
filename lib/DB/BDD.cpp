b6bb8720e2ec1a260f0dedd18267087bb0db2ab2
“Aymeric <aymeric.lavedrine@etu.univ-amu.fr>
2020-04-08 16:49:38 +0200

    Added deleteEntry for Job

--- lib/DB/BDD.cpp
+++ lib/DB/BDD.cpp
@@ -652,4 +652,37 @@ void deleteEntry(Employee &e)
 
 void deleteEntry(Job &j)
 {
+    string filenameIn = dbPath + "/jobs.csv";
+    string filenameOut = dbPath + "/tmp.csv";
+    const char * cIn = filenameIn.c_str();
+    const char * cOut = filenameOut.c_str();
+    ifstream db(cIn);
+    ofstream dbNew;
+    dbNew.open(cOut, ios::out);
+
+    vector<string> dataLine;
+    string row, data, temp;
+    int companyId = c.getId(), id;
+
+    getline(db, temp) ;
+    dbNew << temp << "\n";   
+    while (getline(db, row))
+    {
+        dataLine.clear();
+        stringstream s(row) ;
+
+        while (getline(s, data, ',')) {
+            dataLine.push_back(data) ;
+        }
+
+        id = stoi(dataLine[0]) ;
+        if (companyId != id) {
+            dbNew << row << "\n";
+        }
+    }
+
+    db.close();
+    dbNew.close();
+    remove(cIn);
+    rename(cOut, cIn);
 }
