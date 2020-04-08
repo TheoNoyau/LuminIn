93c81fad91b04504f4db2d4ab46987ad4a13e9e4
“Aymeric <aymeric.lavedrine@etu.univ-amu.fr>
2020-04-08 14:43:46 +0200

    Changed the way we access the files for the get and create methods, now file is opened in the function and file path is given in main

--- lib/DB/BDD.h
+++ lib/DB/BDD.h
@@ -11,31 +11,34 @@ class Company ;
 class Job ;
 class Employee ;
 
+// Sets the path to DB files
+void setPath(std::string path);
+
 // Read all the companies from the companies database table
 // Return a vector of those Companies
-std::vector<Company> getCompanies (std::fstream& db) ;
+std::vector<Company> getCompanies() ;
 
 // Read all the JobSeekers from the jobseekers database table
 // Return a vector of those JobSeekers
-std::vector<JobSeeker> getJobSeekers (std::fstream& db, std::fstream& dbEmployees, std::fstream& dbCompanies) ;
+std::vector<JobSeeker> getJobSeekers() ;
 
 // Read all the Jobs from the jobs database table
 // Return a vector of those Jobs
-std::vector<Job> getJobs (std::fstream& db, std::fstream& dbCompanies) ;
+std::vector<Job> getJobs () ;
 
 // Read all the Employees from the employees database table
 // Return a vector of those Employees
-std::vector<Employee> getEmployees (std::fstream& db, std::fstream& dbCompanies) ;
+std::vector<Employee> getEmployees () ;
 
 // Retruns the company object corresponding to the id given as a parameter
-Company getCompany(std::fstream& db, int const id) ;
+Company getCompany(int const id) ;
 
 // Create a line in the database table
 // The database table will differ according to the object given
-void createEntry (std::fstream &db, Company &c) ;
-void createEntry (std::fstream &db, JobSeeker &js) ;
-void createEntry (std::fstream &db, Employee &e) ;
-void createEntry (std::fstream &db, Job &j) ;
+void createEntry (Company &c) ;
+void createEntry (JobSeeker &js) ;
+void createEntry (Employee &e) ;
+void createEntry (Job &j) ;
 
 // Update/modify a line in the database table 
 // The database table will differ according to the object given
